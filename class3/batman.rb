d5a79ef6baab6cecc61a0cbc2a2760fa85fc847b
Celia Ho <celia.ho@gmail.com>
2015-05-31 22:17:56 -0400

    batman.rb updated—Class 4 hw IP

--- class3/batman.rb
+++ class3/batman.rb
@@ -1,6 +1,9 @@
 contacts = {
+
+# PALS	
+
 	"Batman" => {
-		alignment: good,
+		alignment: "good",
 		real_name: "Bruce Wayne",
 		aliases: [
 			"Insider",
@@ -8,13 +11,13 @@ contacts = {
 			"Robin"
 		],
 		location: "Gotham City, NJ, U.S.A.",
-		occupation: industrialist,
-		marital_status: single,
-		gender: male,
+		occupation: "industrialist",
+		marital_status: "single",
+		gender: "male",
 		height: "6ft 2in",
 		weight: "210 lbs",
-		eyes: blue,
-		hair: black,
+		eyes: "blue",
+		hair: "black",
 		strengths: [
 			"genius-level intellect",
 			"physical prowess",
@@ -23,53 +26,27 @@ contacts = {
 			"wealth"
 		],
 		weaknesses: [
-			"paranoid",
-			"obsessive",
+			"paranoia",
+			"obsessiveness",
 			"difficulty trusting others"
 		]
 	},
-	"Catwoman" => {
-		alignment: neutral,
-		real_name: "Selina Kyle",
-		aliases: [
-			"Irena Dubrovna",
-			"Plundering Kitten",
-			"Princess of Plunder"
-		],
-		location: "Gotham City, NJ, U.S.",
-		occupation: thief,
-		marital_status: single,
-		gender: female,
-		height: "5ft 7in",
-		weight: "133 lbs",
-		eyes: green,
-		hair: black,
-		strengths: [
-			"acrobatic prowess",
-			"advanced hand-to-hand combat",
-			"thievery",
-			"disguise"
-		],
-		weaknesses: [
-			"daughter Helena",
-			"vulnerability to Batman's charm"		]
-	},
 	"Spider-Man" => {
-		alignment: good,
+		alignment: "good",
 		real_name: "Peter Benjamin Parker",
 		aliases: [
 			"Spidey",
 			"Webslinger",
-			"Wall-crawler"
+			"Wall-Crawler"
 		],
-		location: "Metro City, IL, U.S.A.",
-		occupation: "scientist & inventor",
-		marital_status: ,
-		gender: male,
+		location: "New York City, NY, U.S.A.",
+		occupation: "various: ivigilante, inventor, & scientist",
+		marital_status: "single",
+		gender: "male",
 		height: "5ft 10in",
 		weight: "167 lbs",
-		eyes: hazel,
-		hair: brown,
+		eyes: "hazel",
+		hair: "brown",
 		strengths: [
 			"superhuman strength",
 			"superhuman kinesthetics",
@@ -80,10 +57,12 @@ contacts = {
 		weaknesses: [
 			"corny jokes",
 			"ability to cognitively override autonomic reflexes",
+			"doesn't always take his fights seriously",
+			"spider sense can be blocked"
 		]
 	},
 	"Wonder Woman" => {
-		alignment: good,
+		alignment: "good",
 		real_name: "Diana of Themyscira",
 		aliases: [
 			"Diana Prince",
@@ -92,54 +71,25 @@ contacts = {
 		],
 		location: "various: Washington, D.C., U.S.A. & Themyscira (Paradise Island)",
 		occupation: "various: government agent & ambassador",
-		marital_status: single,
-		gender: female,
+		marital_status: "single",
+		gender: "female",
 		height: "6ft 0in",
 		weight: "130 lbs",
-		eyes: blue,
-		hair: black,
+		eyes: "blue",
+		hair: "black",
 		strengths: [
 			"superhuman strength",
 			"superhuman speed",
 			"superhuman durability",
 			"flight",
-			"wisdom of Athena",
-			"advanced hand-to-hand combat"
+			"wisdom of Athena"
 		],
 		weaknesses: [
-			"vulnerability to piercing weapons",
-			"blindness (formerly)"
-		]
-	},
-	"Iron Man" => {
-		alignment: "neutral",
-		real_name: "Anthony Edward 'Tony' Stark",
-		aliases: [
-			"'Shellhead'",
-			"'Golden Avenger'",
-			"Hogan Potts"
-		],
-		location: "various: San Francisco, CA, U.S.A.; New York, NY, U.S.A.; ",
-		occupation: "various: adventurer; President Emeritus, Stark Industries; Founder, Maria Stark Foundation",
-		marital_status: ,
-		gender: male,
-		height: "6ft 1in",
-		weight: "225 lbs",
-		eyes: blue,
-		hair: black,
-		strengths: [
-			"genius-level intellect",
-			"ability to invent sophisticated devices",
-			"keen business mind",
-			"wealth"
-		],
-		weaknesses: [
-			"severe heart injury (formerly)",
-			"Pepper Potts"
+			"vulnerability to piercing weapons"
 		]
 	},
 	"Superman" => {
-		alignment: good,
+		alignment: "good",
 		real_name: "Kal-El",
 		aliases: [
 			"Clark Joseph Kent",
@@ -147,18 +97,17 @@ contacts = {
 			"Man of Steel"
 		],
 		location: "various: Metropolis, NY, U.S.A. & Washington, D.C., U.S.A.",
-		occupation: reporter,
-		marital_status: married,
-		gender: male,
+		occupation: "reporter",
+		marital_status: "married",
+		gender: "male",
 		height: "6ft 3in",
 		weight: "235 lbs",
-		eyes: blue,
-		hair: black,
+		eyes: "blue",
+		hair: "black",
 		strengths: [
 			"superhuman strength",
 			"superhuman speed",
-			"heat vision",
-			"x-vision",
+			"x-ray vision",
 			"ice breath",
 			"flight"
 		],
@@ -170,78 +119,76 @@ contacts = {
 		]
 	},
 
-	# NEUTRAL/BAD
+# NEUTRAL
 	
-	"Lex Luthor" => {
-		alignment: bad,
-		real_name: "Alexander Joseph Luthor",
+	# "Iron Man" => {
+	# 	alignment: "neutral",
+	# 	real_name: "Anthony Edward 'Tony' Stark",
+	# 	aliases: [
+	# 		"'Shellhead'",
+	# 		"'Golden Avenger'",
+	# 		"Hogan Potts"
+	# 	],
+	# 	location: "various: San Francisco, CA, U.S.A.; New York, NY, U.S.A.",
+	# 	occupation: "various: mechanical engineer, inventor, industrialist",
+	# 	marital_status: "single",
+	# 	gender: "male",
+	# 	height: "6ft 1in",
+	# 	weight: "225 lbs",
+	# 	eyes: "blue",
+	# 	hair: "black",
+	# 	strengths: [
+	# 		"genius-level intellect",
+	# 		"ability to invent sophisticated devices",
+	# 		"keen business mind",
+	# 		"wealth"
+	# 	],
+	# 	weaknesses: [
+	# 		"body function dependent on R.T. node",
+	# 		"Pepper Potts"
+	# 	]
+	# },
+	"Catwoman" => {
+		alignment: "neutral",
+		real_name: "Selina Kyle",
 		aliases: [
-			"Mockingbird"
-		],
-		location: "Metropolis, NY, U.S.A.", 
-		occupation: "various: businessman & scientist",
-		marital_status: widowed,
-		gender: male,
-		height: "6ft 2in",
-		weight: "210 lbs",
-		eyes: green,
-		hair: bald,
-		strengths: [
-			"genius-level intellect",
-			"utter ruthlessness",
-			"basic hand-to-hand combat",
-			"shrewd business sense"
-		],
-		weaknesses: [
-			"arrogance",
-			"inability to comprehend enemies"
-		]
-	},
-	"The Joker" => {
-		alignment: bad,
-		real_name: "unknown",
-		aliases: [
-			"Domino Killer",
-			"Joe Kerr",
-			"Jack Napier"
+			"Irena Dubrovna",
+			"Plundering Kitten",
+			"Princess of Plunder"
 		],
 		location: "Gotham City, NJ, U.S.A.",
-		occupation: "unknown",
-		marital_status: widowed,
-		gender: male,
-		height: "6ft 5in",
-		weight: "192 lbs",
-		eyes: green,
-		hair: green,
+		occupation: "thief",
+		marital_status: "single",
+		gender: "female",
+		height: "5ft 7in",
+		weight: "133 lbs",
+		eyes: "green",
+		hair: "black",
 		strengths: [
-			"complete unpredictability",
-			"genius-level intellect",
-			"pain resistance",
-			"toxic blood",
-			"immunity to Joker venom & similar toxins", 
-			"ability to cheat death",
-			"cosmic awareness"
+			"acrobatic prowess",
+			"advanced hand-to-hand combat",
+			"thievery",
+			"disguise"
 		],
 		weaknesses: [
-			"mental illness"
+			"daughter Helena",
+			"vulnerability to Batman's charm"
 		]
 	},
 	"Poison Ivy" => {
-		alignment: neutral,
+		alignment: "neutral",
 		real_name: "Pamela Lillian Isley",
 		aliases: [
-			"",
-			" ",
-			" "
+			"n/a"
 		],
 		location: "Gotham City, NJ, U.S.A.",
-		occupation: scientist,
-		marital_status: single,
-		gender: female,
+		occupation: "scientist",
+		marital_status: "single",
+		gender: "female",
 		height: "5ft 6in",
 		weight: "110 lbs",
-		eyes: green,
-		hair: red,
+		eyes: "green",
+		hair: "red",
 		strengths: [
 			"chlorokinesis",
 			"toxikinesis",
@@ -254,96 +201,153 @@ contacts = {
 			"vulnerability to darkness"
 		]
 	},
+	"Magneto" => {
+		alignment: "neutral",
+		real_name: "Max Eisenhardt",
+		aliases: [
+			"Erik Magnus Lehnsherr",
+			"Prisoner #214782",
+			"Michael Xavier"
+		],
+		location: "various: Genosha, New York, U.S.A.; Asteroild M",
+		occupation: "various: would-be conqueror & mutant rights activist",
+		marital_status: "widowed",
+		gender: "male",
+		height: "6ft 2in",
+		weight: "190 lbs",
+		eyes: "gray",
+		hair: "bald",
+		strengths: [
+			"magnetokinesis",
+			"telepathic resistance",
+			"genius-level intellect",
+			"master strategist",
+			"indomitable will"
+		],
+		weaknesses: [
+			"superpowers dependent on physical condition",
+			"vulnerability to physical & mental fatigue",
+			"propensity for mental illness"
+		]
+	},
+
+# NAUGHTY MONKEYS
+
+	"Lex Luthor" => {
+		alignment: "bad",
+		real_name: "Alexander Joseph Luthor",
+		aliases: [
+			"Mockingbird"
+		],
+		location: "Metropolis, NY, U.S.A.", 
+		occupation: "various: businessman, scientist",
+		marital_status: "widowed",
+		gender: "male",
+		height: "6ft 2in",
+		weight: "210 lbs",
+		eyes: "green",
+		hair: "none",
+		strengths: [
+			"genius-level intellect",
+			"utter ruthlessness",
+			"basic hand-to-hand combat",
+			"shrewd business sense",
+			"wealth"
+		],
+		weaknesses: [
+			"arrogance",
+			"inability to comprehend enemies"
+		]
+	},
+	"The Joker" => {
+		alignment: "bad",
+		real_name: "unknown",
+		aliases: [
+			"Domino Killer",
+			"Joe Kerr",
+			"Jack Napier"
+		],
+		location: "Gotham City, NJ, U.S.A.",
+		occupation: "unknown",
+		marital_status: "widowed",
+		gender: "male",
+		height: "6ft 5in",
+		weight: "192 lbs",
+		eyes: "green",
+		hair: "green",
+		strengths: [
+			"complete unpredictability",
+			"genius-level intellect",
+			"pain resistance",
+			"immunity to Joker venom & similar toxins", 
+			"cosmic awareness"
+		],
+		weaknesses: [
+			"mental illness"
+		]
+	},
 	"Shredder" => {
-		alignment: 
+		alignment: "bad",
 		real_name: "Oroku Saki",
 		aliases: [
-			" ",
-			" ",
-			" "
+			"Ch'rell",
+			"Shred Head",
+			"Oroku Karai"
 		],
-		location: " ",
-		occupation: ,
-		marital_status: ,
-		gender: male,
-		height: " ",
-		weight: "  lbs",
-		eyes: ,
-		hair: ,
+		location: "New York City, NY, U.S.A.",
+		occupation: "leader of Foot Clan ninjas",
+		marital_status: "single",
+		gender: "male",
+		height: "6ft 0in",
+		weight: "200 lbs",
+		eyes: "brown",
+		hair: "black",
 		strengths: [
-			" ",
-			" ",
-			" "
+			"peak physical condition",
+			"hand-to-hand combat",
+			"genius-level intelligence",
+			"weapon mastery",
+			"master strategist"
 		],
 		weaknesses: [
-			" ",
-			" ",
-			" "
-		]
-	},
-	"Magneto" => {
-		alignment: 
-		real_name: "Erik Lehnsherr",
-		aliases: [
-			" ",
-			" ",
-			" "
-		],
-		location: "various: Genosha, New York, U.S.A. & Asteroild M",
-		occupation: ,
-		marital_status: ,
-		gender: male,
-		height: " ",
-		weight: "  lbs",
-		eyes: ,
-		hair: ,
-		strengths: [
-			"",
-			" ",
-			" "
-		],
-		weaknesses: [
-			"",
-			" ",
-			" "
+			"obsession with revenge",
+			"ego",
+			"incompetent henchmen"
 		]
 	}
 }
 
-	
+# CLASS 3 HOMEWORK
+
+puts "About whom are you inquiring, sir?"
+name = gets.chomp
+
+puts ""
+puts "#{name}'s profile: #{contacts["#{name}"]}"
+
+puts ""
+puts "All profiles:"
+puts contacts
+
+
+# CLASS 4 HOMEWORK
+
+puts ""
+
+puts "Name the first contact, sir."
+contact1 = gets.chomp
+
+puts "And the second contact?"
+contact2 = gets.chomp
+
+def contact_compare(contact1, contact2)
+	puts "#{contact1}\''s strengths are #{contacts[:"#{contact1}"][:strengths]} and #{contact2}\''s strengths are #{contacts[:"#{contact2}"][:strengths]}."
+	puts ""
+	puts "#{contact1}\''s weaknesses are #{contacts[:"#{contact1}"][:weaknesses]} and #{contact2}\''s weaknesses are #{contacts[:"#{contact2}"][:weaknesses]}."
+end
+
+contact_compare(contact1, contact2)
 
 
 
 
-user = {
-	name: "Jen", 
-	age: 28,
-	favorite_candy: [
-		"reeses",
-		"almond joy",
-		"lollipops"
-	],
-	location: "New York"
-}
-
-
-users = {
-	"Jen" => {
-	age: 28,
-	favorite_candy: [
-		"reeses",
-		"almond joy",
-		"lollipops"
-	],
-	location: "New York"
-	},
-	"Jon" => {
-	age: 28,
-	favorite_candy: [
-		"gummy bears",
-		"gumdrops",
-		"toblerone"
-	]
-	location: "New York"
-	}
-}
