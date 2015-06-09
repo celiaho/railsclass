3c3f006996dbf14ec7be22b373a0a8296292dba5
Celia Ho <celia.ho@gmail.com>
2015-06-09 04:30:58 -0400

    refined Batman Contact Database hw

--- class3/batman.rb
+++ class3/batman.rb
@@ -4,18 +4,18 @@
 
 	"batman" => {
 		display_name: "Batman",
-		alignment: "heaven-bound",
+		alignment: "nice",
 		real_name: "Bruce Wayne",
 		aliases: [
 			"Insider",
 			"Matches Malone",
 			"Robin"
 		],
-		location: "Gotham City, NJ, U.S.A.",
+		location: "Gotham City, NJ, USA",
 		occupation: "industrialist",
 		marital_status: "single",
 		gender: "male",
-		height: "6ft 2in",
+		height: "6 ft 2 in",
 		weight: "210 lbs",
 		eyes: "blue",
 		hair: "black",
@@ -34,18 +34,18 @@
 	},
 	"spiderman" => {
 		display_name: "Spider-Man",
-		alignment: "heaven-bound",
+		alignment: "nice",
 		real_name: "Peter Benjamin Parker",
 		aliases: [
 			"Spidey",
 			"Webslinger",
 			"Wall-Crawler"
 		],
-		location: "New York City, NY, U.S.A.",
-		occupation: "various: ivigilante, inventor, & scientist",
+		location: "New York City, NY, USA",
+		occupation: "vigilante, inventor, & scientist",
 		marital_status: "single",
 		gender: "male",
-		height: "5ft 10in",
+		height: "5 ft 10 in",
 		weight: "167 lbs",
 		eyes: "hazel",
 		hair: "brown",
@@ -58,25 +58,25 @@
 		],
 		weaknesses: [
 			"corny jokes",
-			"ability to cognitively override autonomic reflexes",
-			"doesn't always take his fights seriously",
-			"spider sense can be blocked"
+			"ability to override autonomic reflexes",
+			"doesn't always take fights seriously",
+			"spider sense vulnerable to blocking"
 		]
 	},
 	"wonderwoman" => {
 		display_name: "Wonder Woman",
-		alignment: "heaven-bound",
+		alignment: "nice",
 		real_name: "Diana of Themyscira",
 		aliases: [
 			"Diana Prince",
 			"Princess Diana",
 			"Miss America"
 		],
-		location: "various: Washington, D.C., U.S.A. & Themyscira (Paradise Island)",
-		occupation: "various: government agent & ambassador",
+		location: "Washington, D.C., USA & Themyscira (Paradise Island)",
+		occupation: "government agent & ambassador",
 		marital_status: "single",
 		gender: "female",
-		height: "6ft 0in",
+		height: "6 ft 0 in",
 		weight: "130 lbs",
 		eyes: "blue",
 		hair: "black",
@@ -93,18 +93,18 @@
 	},
 	"superman" => {
 		display_name: "Superman",
-		alignment: "heaven-bound",
+		alignment: "nice",
 		real_name: "Kal-El",
 		aliases: [
 			"Clark Joseph Kent",
 			"Commander El",
 			"Man of Steel"
 		],
-		location: "various: Metropolis, NY, U.S.A. & Washington, D.C., U.S.A.",
+		location: "Metropolis, NY, USA & Washington, D.C., USA",
 		occupation: "reporter",
 		marital_status: "married",
 		gender: "male",
-		height: "6ft 3in",
+		height: "6 ft 3 in",
 		weight: "235 lbs",
 		eyes: "blue",
 		hair: "black",
@@ -134,11 +134,11 @@
 			"'Golden Avenger'",
 			"Hogan Potts"
 		],
-		location: "various: San Francisco, CA, U.S.A.; New York, NY, U.S.A.",
-		occupation: "various: mechanical engineer, inventor, industrialist",
+		location: "San Francisco, CA, USA; New York, NY, USA",
+		occupation: "mechanical engineer, inventor, industrialist",
 		marital_status: "single",
 		gender: "male",
-		height: "6ft 1in",
+		height: "6 ft 1 in",
 		weight: "225 lbs",
 		eyes: "blue",
 		hair: "black",
@@ -154,7 +154,7 @@
 		]
 	},
 	"catwoman" => {
-		display_name: "Catman",
+		display_name: "Catwoman",
 		alignment: "neutral",
 		real_name: "Selina Kyle",
 		aliases: [
@@ -162,11 +162,11 @@
 			"Plundering Kitten",
 			"Princess of Plunder"
 		],
-		location: "Gotham City, NJ, U.S.A.",
+		location: "Gotham City, NJ, USA",
 		occupation: "thief",
 		marital_status: "single",
 		gender: "female",
-		height: "5ft 7in",
+		height: "5 ft 7 in",
 		weight: "133 lbs",
 		eyes: "green",
 		hair: "black",
@@ -186,13 +186,13 @@
 		alignment: "neutral",
 		real_name: "Pamela Lillian Isley",
 		aliases: [
-			"n/a"
+			"none"
 		],
-		location: "Gotham City, NJ, U.S.A.",
+		location: "Gotham City, NJ, USA",
 		occupation: "scientist",
 		marital_status: "single",
 		gender: "female",
-		height: "5ft 6in",
+		height: "5 ft 6 in",
 		weight: "110 lbs",
 		eyes: "green",
 		hair: "red",
@@ -217,11 +217,11 @@
 			"Prisoner #214782",
 			"Michael Xavier"
 		],
-		location: "various: Genosha, New York, U.S.A.; Asteroild M",
-		occupation: "various: would-be conqueror & mutant rights activist",
+		location: "Genosha, New York, USA; Asteroid M",
+		occupation: "would-be conqueror & mutant rights activist",
 		marital_status: "widowed",
 		gender: "male",
-		height: "6ft 2in",
+		height: "6 ft 2 in",
 		weight: "190 lbs",
 		eyes: "gray",
 		hair: "bald",
@@ -243,16 +243,16 @@
 
 	"lexluthor" => {
 		display_name: "Lex Luthor",
-		alignment: "naughty monkey",
+		alignment: "naughty",
 		real_name: "Alexander Joseph Luthor",
 		aliases: [
 			"Mockingbird"
 		],
-		location: "Metropolis, NY, U.S.A.", 
-		occupation: "various: businessman, scientist",
+		location: "Metropolis, NY, USA", 
+		occupation: "businessman, scientist",
 		marital_status: "widowed",
 		gender: "male",
-		height: "6ft 2in",
+		height: "6 ft 2 in",
 		weight: "210 lbs",
 		eyes: "green",
 		hair: "none",
@@ -268,20 +268,20 @@
 			"inability to comprehend enemies"
 		]
 	},
-	"thejoker" => {
+	"joker" => {
 		display_name: "The Joker",
-		alignment: "naughty monkey",
+		alignment: "naughty",
 		real_name: "unknown",
 		aliases: [
 			"Domino Killer",
 			"Joe Kerr",
 			"Jack Napier"
 		],
-		location: "Gotham City, NJ, U.S.A.",
+		location: "Gotham City, NJ, USA",
 		occupation: "unknown",
 		marital_status: "widowed",
 		gender: "male",
-		height: "6ft 5in",
+		height: "6 ft 5 in",
 		weight: "192 lbs",
 		eyes: "green",
 		hair: "green",
@@ -299,18 +299,18 @@
 	},
 	"shredder" => {
 		display_name: "Shredder",
-		alignment: "naughty monkey",
+		alignment: "naughty",
 		real_name: "Oroku Saki",
 		aliases: [
 			"Ch'rell",
 			"Shred Head",
 			"Oroku Karai"
 		],
-		location: "New York City, NY, U.S.A.",
+		location: "New York City, NY, USA",
 		occupation: "leader of Foot Clan ninjas",
 		marital_status: "single",
 		gender: "male",
-		height: "6ft 0in",
+		height: "6 ft 0 in",
 		weight: "200 lbs",
 		eyes: "brown",
 		hair: "black",
@@ -329,38 +329,160 @@
 	}
 }
 
-# CLASS 3 HOMEWORK
 
-puts "About whom are you inquiring, sir?"
-name = gets.chomp.downcase.delete(" .!-")
+# # CLASS 3 HOMEWORK
 
-puts ""
-puts "#{name}'s profile: #{@contacts[name]}"
+# puts "About whom are you inquiring, sir?"
+# name = gets.chomp.downcase.delete(" .!-")
 
-puts ""
-puts "All profiles:"
-puts @contacts
+# puts ""
+# puts "#{name}'s profile: #{@contacts[name]}"
+
+# puts ""
+# puts "All profiles:"
+# puts @contacts
 
 
-# CLASS 4 HOMEWORK
+# # CLASS 4 HOMEWORK
 
-puts ""
+# puts ""
 
-puts "Name the first contact, sir."
-contact1 = gets.chomp.downcase.delete(" .!-")
+# puts "Name the first contact, sir."
+# contact1 = gets.chomp.downcase.delete(" .!-")
 
-puts "And the second contact?"
-contact2 = gets.chomp.downcase.delete(" .!-")
+# puts "And the second contact?"
+# contact2 = gets.chomp.downcase.delete(" .!-")
 
-def sw_versus(contact1, contact2)
-	puts "#{contact1}\'s strengths are #{@contacts[contact1][:strengths]} and #{contact2}\'s strengths are #{@contacts[contact2][:strengths]}."
-	puts ""
-	puts "#{contact1}\'s weaknesses are #{@contacts[contact1][:weaknesses]} and #{contact2}\'s weaknesses are #{@contacts[contact2][:weaknesses]}."
+# def sw_versus(contact1, contact2)
+# 	puts "#{contact1}\'s strengths are #{@contacts[contact1][:strengths]} and #{contact2}\'s strengths are #{@contacts[contact2][:strengths]}."
+# 	puts ""
+# 	puts "#{contact1}\'s weaknesses are #{@contacts[contact1][:weaknesses]} and #{contact2}\'s weaknesses are #{@contacts[contact2][:weaknesses]}."
+# end
+
+# puts 
+# sw_versus(contact1, contact2)
+
+
+# CLASS 3 & 4 HOMEWORK
+
+
+# ——> Main Menu
+puts
+puts "*** BATABASE CENTRAL ***"
+puts "Initializing..."
+sleep 0.5
+
+puts "Press 1 for individual lookup."
+puts "Press 2 for contact comparison."
+puts "Press 3 for BATabase directory."
+print ">> "
+
+# ——> Main Menu: User Input
+central_command = gets.chomp
+
+if central_command == "1" 
+		
+	puts
+	puts "*** BATABASE INDIVIDUAL LOOKUP ***"
+	puts "About whom are you inquiring, sir?"
+	print ">> "
+	profile_name = gets.chomp.downcase.delete(" .!-")
+
+	@contacts.each do |contact, attributes|
+		if contact == "#{profile_name}"
+			puts
+			puts "#{@contacts[contact][:display_name]}\'s Profile"
+			puts "   Disposition: #{@contacts[contact][:alignment]}"
+			puts "   Real Name: #{@contacts[contact][:real_name]}"
+			puts "   Aliases: #{@contacts[contact][:aliases]}"
+			puts "   Location: #{@contacts[contact][:location]}"
+			puts "   Marital Status: #{@contacts[contact][:marital_status]}"
+			puts "   Gender: #{@contacts[contact][:gender]}"
+			puts "   Height: #{@contacts[contact][:height]}"
+			puts "   Weight: #{@contacts[contact][:weight]}"
+			puts "   Eyes: #{@contacts[contact][:eyes]}"
+			puts "   Hair: #{@contacts[contact][:hair]}"
+			puts "   Strengths: #{@contacts[contact][:strengths]}"
+			puts "   Weaknesses: #{@contacts[contact][:weaknesses]}"
+			puts
+		# else
+		# 	puts 
+		# 	puts "Contact not recognized. Refer to BATabase directory."
+		# 	puts
+		end
+	end
+
+elsif central_command == "2"
+		
+	puts
+	puts "*** BATABASE CONTACT COMPARISON ***"
+	puts "Name the first contact, sir."
+	print ">> "
+	comparison_name_1 = gets.chomp.downcase.delete(" .!-")
+
+	puts "And the second contact?"
+	print ">> "
+	comparison_name_2 = gets.chomp.downcase.delete(" .!-")
+
+	def sw_versus(comparison_name_1, comparison_name_2)
+		
+		# Comparison of strengths
+		puts "#{@contacts[comparison_name_1][:display_name]}\'s Strengths:"
+		@contacts[comparison_name_1][:strengths].each do |attribute|
+			puts "   #{attribute}"
+		end
+		puts "#{@contacts[comparison_name_2][:display_name]}\'s Strengths:"
+		@contacts[comparison_name_2][:strengths].each do |attribute|
+			puts "   #{attribute}"
+		end
+		
+		puts  # line break
+
+		# Comparison of weaknesses
+		puts "#{@contacts[comparison_name_1][:display_name]}\'s Weaknesses:"
+		@contacts[comparison_name_1][:weaknesses].each do |attribute|
+			puts "   #{attribute}"
+			# puts "   #{@contacts[comparison_name_1][:weaknesses]}"
+			# This works too, but gives results in ugly array formatting.
+			# The loop makes for cleaner copy.
+		end
+		puts "#{@contacts[comparison_name_2][:display_name]}\'s Weaknesses:"
+		@contacts[comparison_name_2][:weaknesses].each do |attribute|
+			puts "   #{attribute}"
+		end
+		
+	end
+
+	puts 
+	sw_versus(comparison_name_1, comparison_name_2)
+	puts
+
+elsif central_command == "3"
+
+	puts
+	puts "*** BATABASE CONTENTS ***"
+	puts
+	# puts @contacts    ——> dumps info in hard-to-read hash format
+	# Code below outputs more legibly.
+	@contacts.each do |contact, attributes|
+		puts "#{@contacts[contact][:display_name]}"
+		puts "   Disposition: #{@contacts[contact][:alignment]}"
+		puts "   Real Name: #{@contacts[contact][:real_name]}"
+		puts "   Aliases: #{@contacts[contact][:aliases]}"
+		puts "   Location: #{@contacts[contact][:location]}"
+		puts "   Marital Status: #{@contacts[contact][:marital_status]}"
+		puts "   Gender: #{@contacts[contact][:gender]}"
+		puts "   Height: #{@contacts[contact][:height]}"
+		puts "   Weight: #{@contacts[contact][:weight]}"
+		puts "   Eyes: #{@contacts[contact][:eyes]}"
+		puts "   Hair: #{@contacts[contact][:hair]}"
+		puts "   Strengths: #{@contacts[contact][:strengths]}"
+		puts "   Weaknesses: #{@contacts[contact][:weaknesses]}"
+		puts
+	end
+	
+else 
+
+	puts "Invalid input. Try again."
+
 end
-
-puts 
-sw_versus(contact1, contact2)
-
-
-
-
